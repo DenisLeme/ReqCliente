f7e349fc70925361f3760f44567df7a33ecaace5
Denis Leme <denis_leme@icloud.com>
2023-07-26 01:23:57 -0300

    Migration e alterações no modal

---
+++ TesteCliente/Migrations/20230726041003_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TesteCliente.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cliente = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TipoCliente = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NomeContato = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelefoneContato = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bairro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Logradouro = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataAtualizacao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.ClienteId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
--- TesteCliente/Models/Clientes.cs
+++ TesteCliente/Models/Clientes.cs
@@ -1,18 +1,27 @@
-﻿namespace TesteCliente.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TesteCliente.Models
 {
     public class Clientes
     {
-        public decimal ClienteId { get; set; }
+        [Key]
+        public int ClienteId { get; set; }
         public string? Cliente { get; set; }
         public string TipoCliente { get; set; }
         public string NomeContato { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal TelefoneContato { get; set; }
+
         public string Cidade { get; set; }
         public string Bairro { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Logradouro { get; set; }
+
         public DateTime DataCadastro { get; set; }
         public DateTime DataAtualizacao { get; set; }
-
-
     }
 }
